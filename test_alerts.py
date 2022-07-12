c5ddcdda69590addfabfd67db18223e89eb2fb01
Filippo Giunchedi <filippo@wikimedia.org>
2022-07-12 17:23:53 +0200

    Test for unquoted query strings in runbook/dashboard
    Catch most common/likely mistakes when copy/pasting URLs into
    dashboard/runbook annotations.
    
    Bug: T312817
    Change-Id: I9cd4ea10185eabb48333ff1f5c4a35c5e7e14933

--- test_alerts.py
+++ test_alerts.py
@@ -11,8 +11,10 @@ import pathlib
 import re
 import string
 import subprocess
+import urllib
 import warnings
 from pathlib import Path
+from urllib.parse import quote, unquote
 
 import pytest
 import requests
@@ -183,11 +185,66 @@ def _validate_rule_metadata(rule):
         "Alert names with spaces are hard to address and silence: %r" % alertname
     )
 
+    for a in ("runbook", "dashboard"):
+        if a in annotations:
+            assert _url_is_quoted(annotations[a]), (
+                "URL in %s contains unquoted characters, check warnings for details" % a
+            )
+
     for a in wanted_annotations:
         if a not in annotations:
             warnings.warn(
                 UserWarning("Annotation %r not found for alert %s" % (a, alertname))
             )
+
+
+def _untemplate(string):
+    """Return string without golang text/template markers."""
+
+    return re.sub("{{.*}}", "", string)
+
+
+def _url_is_quoted(url):
+    """Check if url has been URL quoted. The check will ignore template markers since no quoting can be checked at this stage."""
+    u = urllib.parse.urlparse(url)
+    if not u.query or "=" not in u.query:
+        return True
+
+    # Basic split/parsing of qs variables, urllib.parse.parse_qs unquotes the result, which we don't want
+    qs_vars = u.query.split("&")
+    qs_pairs = [x.split("=", 1) for x in qs_vars]
+    for el in qs_pairs:
+        # qs variable with no value (e.g. 'fullscreen' in Grafana)
+        if len(el) == 1:
+            raw_name = el[0]
+            raw_value = None
+        else:
+            raw_name, raw_value = el
+
+        name = _untemplate(raw_name)
+        if quote(unquote(name)) != name:
+            warnings.warn(
+                UserWarning(
+                    "Unquoted query string variable %r: expected %r"
+                    % (name, quote(name))
+                )
+            )
+            return False
+
+        if raw_value is None:
+            continue
+
+        value = _untemplate(raw_value)
+        if quote(unquote(value)) != value:
+            warnings.warn(
+                UserWarning(
+                    "Unquoted value for query string variable %r. Expected: %r Got: %r"
+                    % (name, quote(value), value)
+                )
+            )
+            return False
+
+    return True
 
 
 def _run_promtool(args, workdir):
