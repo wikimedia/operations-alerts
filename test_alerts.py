39835cfd850d15a12c35674567e9b1dffd5a0ab1
Andrea Denisse Gómez-Martínez <adenisse@wikimedia.org>
2024-03-27 18:40:58 -0600

    Added Apache 2.0 License to repository and test_alerts.py tool
    - Created LICENSE file with Apache 2.0 License text for the repository.
    - Added Apache 2.0 License header to test_alerts.py to ensure proper
      licensing of the tool.
    
    Bug: T361010
    Change-Id: Ice7fb03a0fa552a95952b74c4201a157e0f34961

--- test_alerts.py
+++ test_alerts.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-
+#
+# Copyright 2024 Wikimedia Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Use pytest to validate/test alerting rules files and their tests.
 # For each directory of ALERTSDIR the following tests are ran:
 # - each *_test.yaml file is a alerts unit test file, validate it with promtool
